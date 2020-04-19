5d52e9cdfc03c018d709a52b9c8b07b4d7b8a4f5
Amar Balikai <amar.balikai108@gmail.com>
2020-04-19 11:37:02 +0530

    Update in model class

--- app/src/main/java/com/example/countryinformation/model/InfoModel.kt
+++ app/src/main/java/com/example/countryinformation/model/InfoModel.kt
@@ -2,6 +2,6 @@ package com.example.countryinformation.model
 
 import androidx.room.PrimaryKey
 
-data class InfoModelData (var title:String="No title", var description:String="No description", var imageHref:String) {
+data class InfoModelData (var title:String, var description:String, var imageHref:String) {
 
 }
